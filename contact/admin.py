0801aada81c036b7aa5708f495c1e66fde26bdfd
Eric Ferraz <ericferraz4@gmail.com>
2025-06-13 07:58:37 -0300

    "registra os primeiros contatos"

--- contact/admin.py
+++ contact/admin.py
@@ -3,4 +3,11 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('id','fist_name', 'last_name', 'fone', 'email', 'created_date',)
+    ordering = ("id",)
+
+    search_fields = ("id", "fist_name", "last_name", )
+    list_per_page = 10
+    list_max_show_all = 1000
+    list_editable = ('fist_name', 'last_name', 'fone', 'email',)
+    list_display_links = ('id', ) 
