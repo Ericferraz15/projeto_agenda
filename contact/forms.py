9ae2dd75b8ed9ed7cd9870ab2091431601c51347
Eric Ferraz <ericferraz4@gmail.com>
2025-07-03 08:35:35 -0300

    cria a page update e create com algumas validações

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,6 @@
 
 from contact.models import Contact
-from django import forms #type: ignore
+from django import forms 
 from . import models
 from django.core.exceptions import ValidationError #type: ignore
 
@@ -8,7 +8,10 @@ from django.core.exceptions import ValidationError #type: ignore
 class ContactForms(forms.ModelForm):
     class Meta:
         model = Contact
-        fields = ("first_name","last_name", "phone",)
+        fields = (
+            "first_name","last_name", "phone",
+            "email","description", "Category",
+            )
 
         widgets = {
             "first_name" : forms.TextInput(
@@ -20,6 +23,30 @@ class ContactForms(forms.ModelForm):
         }
 
     def clean(self):
+        first_name = self.cleaned_data.get("first_name")
+        last_name = self.cleaned_data.get("last_name")
+
+        if first_name == last_name:
+            msg = ValidationError(" nome e sobrenome não podem ser iguais", code= "invalid")
+
+            self.add_error("first_name", msg)
+            self.add_error("last_name", msg)
         
         return super().clean()
+    
+    def clean_first_name(self):
+
+        first_name = self.cleaned_data.get("first_name")
+
+        if first_name == "ABC":
+            self.add_error\
+            (
+            "first_name", 
+            ValidationError 
+                (
+                "veio do add_error",
+                code= "invalid"
+                )
+            )
+        return first_name
 
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -5,14 +5,23 @@ from django.shortcuts import render,get_object_or_404, redirect
 from contact.models import Contact
 from django.core.paginator import Paginator 
 from django import forms
-from contact.forms import ContactForms
+from contact.forms import ContactForms, Contact
+from django.urls import reverse
 
 
 def create (request):
+    form_action  = reverse("contact:create")
+    
     if request.method == "POST":
+        form =  ContactForms(request.POST)
         context = {
-                "form" : ContactForms(request.POST)
+                "form" : form,
+                "form_action": form_action
         }
+
+        if form.is_valid(): 
+            contact = form.save()
+            return redirect('contact:update', contact.pk)
 
         return render(
             request, 
@@ -20,10 +29,48 @@ def create (request):
             context
             )
 
-    context = {
-            "form" : ContactForms()
-           }
 
+    context = {
+                "form" : ContactForms(),
+                "form_action": form_action,
+        }
+    return render(
+        request, 
+        "contact/create.html",
+        context
+        )
+
+
+def update (request,contact_id):
+    contact = get_object_or_404(
+        Contact,
+        pk=contact_id,
+        show=True,
+        )
+    form_action  = reverse("contact:update",args=(contact_id,))
+    
+    if request.method == "POST":
+        form =  ContactForms(request.POST , instance=contact)
+        context = {
+                "form" : form,
+                "form_action": form_action
+        }
+
+        if form.is_valid():
+            form.save()
+            return redirect("contact:update", contact_id = contact.pk)
+
+        return render(
+            request, 
+            "contact/create.html",
+            context
+            )
+
+
+    context = {
+                "form" : ContactForms(instance=contact),
+                "form_action": form_action,
+        }
     return render(
         request, 
         "contact/create.html",
